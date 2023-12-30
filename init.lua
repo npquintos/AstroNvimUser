072bd905fb4fbf61ed23dfb84a4567683d769d76
Noel Quintos <npquintos@gmail.com>
2023-12-30 09:47:34 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -11,14 +11,14 @@ return {
     show_changelog = true, -- show the changelog after performing an update
     auto_quit = false, -- automatically quit the current session after a successful update
     remotes = { -- easily add new remotes to track
-      --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
+         ["AstroConfig"] = "https://github.com/npquintos/AstroNvimUser.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     },
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "bamboo",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
