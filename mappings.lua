cc1638f0451dcd65cee0731c48d231c770dd3cac
Noel Quintos <npquintos@gmail.com>
2023-12-30 09:53:02 -0700

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -30,6 +30,21 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<right>"] = { ":let linenum=getpos('.')[1]|:wincmd l|:call cursor(linenum,0)<cr>", desc = "go to same line number at right pane" },
+    ["<left>"] = { ":let linenum=getpos('.')[1]|:wincmd h|:call cursor(linenum,0)<cr>", desc = "go to same line number at left pane" },
+    ["<up>"] = { ":let linenum=getpos('.')[1]|:wincmd k|:call cursor(linenum,0)<cr>", desc = "go to same line number at upper pane" },
+    ["<down>"] = { ":let linenum=getpos('.')[1]|:wincmd j|:call cursor(linenum,0)<cr>", desc = "go to same line number at lower pane" },
+    ["o"] = { "%", desc = "jump to opening/closing pair" },
+    ["T"] = { "zt", desc = "move current line to top" },
+    ["E"] = { "$", desc = "move cursor to end of line" },
+    ["B"] = { "^", desc = "move cursor to beginning of line" },
+    ["P"] = { "]pcf=def<esc>A:<cr>", desc = "convert to function def" },
+    [">"] = { ">gv", desc = "retain selection after indentation" },
+    ["<"] = { "<gv", desc = "retain selection after un-indentation" },
+    ["gf"] = { "?def <cr>w", desc = "go to start of function" },
+    ["gb"] = { "?def <cr>w*", desc = "go to calling function" },
+    ["<A-h>"] = { ":bprev<cr>", desc = "go to previous buffer" },
+    ["<A-l>"] = { ":bnext<cr>", desc = "go to next buffer" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
