2d1f424b761f12828aef41036c201c329a0352b7
Noel Quintos <npquintos@gmail.com>
2024-01-13 09:12:04 -0700

    Add files via upload

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,32 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  ft = "norg",
+  LocalLeader = ";",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  event = "VeryLazy",
+  opts = {
+    load = {
+      ["core.defaults"] = {
+      }, -- Loads default behaviour
+      ["core.concealer"] = {}, -- Adds pretty icons to your documents
+      ["core.keybinds"] = {}, -- Adds default keybindings
+      ["core.completion"] = {
+        config = {
+          engine = "nvim-cmp",
+        },
+      }, -- Enables support for completion plugins
+      ["core.journal"] = {}, -- Enables support for the journal module
+      ["core.dirman"] = { -- Manages Neorg workspaces
+        config = {
+          workspaces = {
+            notes = "~/notes",
+          },
+          default_workspace = "notes",
+        },
+      },
+    },
+  },
 }
