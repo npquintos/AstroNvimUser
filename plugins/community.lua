5cb447a75908a42bb3e72f2f23df1598b6fa0d69
Noel Quintos <npquintos@gmail.com>
2024-10-23 15:21:01 -0600

    Update community.lua
    added coq and bamboo

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,8 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
+  { import = "astrocommunity.completion.coq_nvim" },
+  { import = "astrocommunity.colorscheme.bamboo-nvim" },
 }
