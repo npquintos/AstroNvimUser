48280b2bd598033960f45b506257c2c1cf2b4322
Noel Quintos <npquintos@gmail.com>
2024-10-24 11:12:30 -0600

    disabled cmp-nvim

--- plugins/core.lua
+++ plugins/core.lua
@@ -27,6 +27,7 @@ return {
   },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
+  { "hrsh7th/nvim-cmp", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   -- {
